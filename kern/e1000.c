a884ee47af56aa7d7b3891a1817b49cda7c16437
רבקה בר-סף <rebeccoosh@gmail.com>
2016-06-10 21:13:40 +0300

    ex11

--- kern/e1000.c
+++ kern/e1000.c
@@ -84,7 +84,7 @@ int e1000_recv_packet(char *buffer){
 
 	if(!(rx_desc[rdt].status & DD_BIT)) return -E_NO_RCV;
 
-	if(!(rx_desc[rdt].status & EOP_BIT) panic("no long packet implemnted");
+	if(!(rx_desc[rdt].status & EOP_BIT)) panic("no long packet implemnted");
 
 	memcpy(&buffer, &rx_buffers[rdt], rx_desc[rdt].length);
 	rx_desc[rdt].status &= ~DD_BIT; 
