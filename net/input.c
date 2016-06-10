4dccde141ec13d45bcdddce5de5765651b1d2971
רבקה בר-סף <rebeccoosh@gmail.com>
2016-06-10 21:21:22 +0300

    ex11

--- net/input.c
+++ net/input.c
@@ -16,13 +16,11 @@ input(envid_t ns_envid)
 	// another packet in to the same physical page.
 	char buffer[RECV_SIZE];
 	int perm = PTE_U | PTE_P | PTE_W;
-	int len;
 
 	for(;;){
-		if((len = recv_packet(buffer, RECV_SIZE -1))){
-
+			
+			int len = recv_packet(buffer, RECV_SIZE -1);
 			//while((r=sys_page_alloc(0, &nsipcbuf, perm))<0)
-
 			nsipcbuf.pkt.jp_len = len;
 			memcpy(nsipcbuf.pkt.jp_data, buffer, len);
 			sys_ipc_try_send(ns_envid, NSREQ_INPUT, &nsipcbuf ,perm);
