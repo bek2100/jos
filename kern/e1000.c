8321121254ebfa2eef6cea96ca23cce0cba00e5b
רבקה בר-סף <rebeccoosh@gmail.com>
2016-06-17 15:51:33 +0300

    intr

--- kern/e1000.c
+++ kern/e1000.c
@@ -133,7 +133,7 @@ int e1000_try_recv_packet(void *page, size_t *out_len)
 
 void e1000_intr()
 {
-	cprintf("e1000 INTR %d\n", bar0[IMS], bar0[ICR]);
+	cprintf("e1000 INTR %d %d\n", bar0[IMS], bar0[ICR]);
 	bar0[ICS] = 1;
 }
 
