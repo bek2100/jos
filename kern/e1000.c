9114599620e7069fe04c4e8334d209f869cfa12a
רבקה בר-סף <rebeccoosh@gmail.com>
2016-06-17 18:20:34 +0300

    intr1

--- kern/e1000.c
+++ kern/e1000.c
@@ -34,7 +34,7 @@ int e1000_attach(struct pci_func *pcif)
 	for (i = 0; i < TX_COUNT; ++i)
 	{
 		tx_desc[i].status |= DD_BIT;
-		tx_desc[tdt].cmd |= RS_BIT;	}
+	}
 
 	for (i = 0; i < RX_COUNT; ++i)
 	{
@@ -133,6 +133,6 @@ int e1000_try_recv_packet(void *page, size_t *out_len)
 
 void e1000_intr()
 {
-	cprintf("e1000 INTR %d %d\n", bar0[IMS], bar0[ICR]);
+	cprintf("e1000 INTR %d %d %d\n", bar0[IMS], bar0[ICR], bar0[ICR]);
 }
 
